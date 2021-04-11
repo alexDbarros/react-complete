5e1a2b188ebaed8f273c1984bf1fb10628ed94cd
alexDbarros <barros.d.alex@gmail.com>
2021-04-11 11:36:05 -0300

    Using the useState() Hook for State Manipulation

--- react-guide/src/App.js
+++ react-guide/src/App.js
@@ -1,35 +1,77 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-import Person from './Person/Person';
+import Person from "./Person/Person";
 
-class App extends Component {
-  state = {
+const app = (props) => {
+  const [personsState, setPersonsState] = useState({
     persons: [
-      {name: 'REx', age: 25},
-      {name: 'PP', age: 12},
-      {name: 'UBCO', age: 22},
-      {name: 'OLSA', age: 36}
-    ]
-  }
+      { name: "REx", age: 25 },
+      { name: "PP", age: 12 },
+      { name: "UBCO", age: 22 },
+      { name: "OLSA", age: 36 },
+    ],
+    otherState: "some value",
+  });
 
+  const switchNameHandler = () => {
+    // console.log('Ok');
+    setPersonsState({
+      persons: [
+        { name: "REx 11", age: 25 },
+        { name: "PP", age: 12 },
+        { name: "UBCO", age: 22 },
+        { name: "OLSA", age: 26 },
+      ],
+    });
+  };
 
-  render() {
-    return (
-      <div className="App">
-        <h1>Hi, I´m a React App </h1>
-        <p>What</p>
-        <button>Switch name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>I like her</Person>
-        <Person name={this.state.persons[3].name} age={this.state.persons[3].age}/>
-      </div>
-    );
-    //return React.createElement('div', {className: 'App'}, React.createElement('h1', null,'Hello word!!'));
-  }
-  
-}
+  return (
+    <div className="App">
+      <h1>Hi, I´m a React App </h1>
+      <p>What</p>
+      <button onClick={switchNameHandler}>Switch name</button>
+      <Person
+        name={personsState.persons[0].name}
+        age={personsState.persons[0].age}
+      />
+      <Person
+        name={personsState.persons[1].name}
+        age={personsState.persons[1].age}
+      />
+      <Person
+        name={personsState.persons[2].name}
+        age={personsState.persons[2].age}
+      >
+        I like her
+      </Person>
+      <Person
+        name={personsState.persons[3].name}
+        age={personsState.persons[3].age}
+      />
+    </div>
+  );
+};
 
-export default App;
+export default app;
+
+// state = {
+//   persons: [
+//     {name: 'REx', age: 25},
+//     {name: 'PP', age: 12},
+//     {name: 'UBCO', age: 22},
+//     {name: 'OLSA', age: 36}
+//   ]
+// }
+
+// switchNameHandler = () => {
+//   // console.log('Ok');
+//   this.setState({
+//     persons: [
+//       {name: 'REx 11', age: 25},
+//       {name: 'PP', age: 12},
+//       {name: 'UBCO', age: 22},
+//       {name: 'OLSA', age: 26}
+//     ]
+//   });
+// };
